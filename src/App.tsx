15d085cad323f0ca9db2d9b5591ed45848d94031
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-19 00:42:44 +0900

    Changed GlobalStyle test

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
+import { GlobalStyle } from './common/global';
 
 export default function App() {
     return (
-        <Routes>
-            <Route element={<MainLayout />}>
-                <Route index element={<Home />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-        </Routes>
+        <>
+            <GlobalStyle />
+            <Routes>
+                <Route element={<MainLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { GlobalStyle } from './common/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
-            <GlobalStyle />
             <App />
         </BrowserRouter>
     </React.StrictMode>,
