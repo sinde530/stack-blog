77c329afe0bd847c19ddbb4fc7e76eabfb0142c1
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-19 00:46:05 +0900

    Add test base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/stack-blog',
   build: {
 	  assetsInlineLimit: 0,
   }
