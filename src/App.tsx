1390bcd311e9423c402abc7733305318dfeec97b
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-19 00:50:07 +0900

    Add test routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
-import { GlobalStyle } from './common/global';
 
 export default function App() {
     return (
-        <>
-            <GlobalStyle />
-            <Routes>
-                <Route element={<MainLayout />}>
+        <Routes>
+            <Route element={<MainLayout />}>
+                <Route path="/stack-blog/" element={<Outlet />}>
                     <Route index element={<Home />} />
                 </Route>
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </>
+            </Route>
+            <Route path="*" element={<NotFound />} />
+        </Routes>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { GlobalStyle } from './common/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
+            <GlobalStyle />
             <App />
         </BrowserRouter>
     </React.StrictMode>,
