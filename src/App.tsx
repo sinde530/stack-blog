620381165ae02aed789e83d42afa1876a118b8ca
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-18 17:31:16 +0900

    Intall

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
 
 function App() {
     const [count, setCount] = useState(0);
@@ -8,9 +7,6 @@ function App() {
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} alt="Vite logo" />
-                </a>
                 <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img src={reactLogo} alt="React logo" />
                 </a>
