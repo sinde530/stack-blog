84fdb0b3d4b8785d7af36307ac325bc81ea59410
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-18 17:11:00 +0900

    Remove css

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-import './index.css';
+import { GlobalStyle } from './common/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <GlobalStyle />
         <App />
     </React.StrictMode>,
 );
