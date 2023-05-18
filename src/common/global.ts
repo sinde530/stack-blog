7c8f78e057886bc29c64decbe5140e7e450157e3
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-18 17:08:36 +0900

    Create

---
+++ src/common/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: 'Roboto', sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        box-sizing: border-box;
+    }
+`;
