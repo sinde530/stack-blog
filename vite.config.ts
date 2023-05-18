fc0e65b23e06a34039ebacf52ba59acd26ec2f3f
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-18 23:38:19 +0900

    Remove `base url`

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/stack-blog',
   build: {
 	  assetsInlineLimit: 0,
   }
