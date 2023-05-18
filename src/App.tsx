846332aaf55e0b0ad2887a76b7204ea972e4f38b
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-18 23:43:10 +0900

    Create `NotFound` Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
             <Route element={<MainLayout />}>
                 <Route index element={<Home />} />
             </Route>
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }
