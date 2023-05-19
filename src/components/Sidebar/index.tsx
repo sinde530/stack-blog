35722e448b5f7b00e2a3264b36a6181ed34a4182
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-19 12:32:03 +0900

    Add Template `Sudebar` Component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,21 +1,64 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div({
-    backgroundColor: 'red',
     width: 'calc(300px - 1em)',
     overflowY: 'auto',
     height: 'calc(100vh - 2em - 2em)',
     padding: '12px 12px',
 });
 
-export const Profile = styled.div({});
+export const Div = styled.div({
+    paddingTop: '1rem',
+    paddingBottom: '1rem',
+});
+
+export const Profile = styled.div({
+    display: 'grid',
+});
 
 export const ProfileImage = styled.img({
     width: '250px',
     height: '180px',
+    borderRadius: '30px',
 });
 
-export const Categories = styled.div({});
+export const A = styled.a({
+    paddingBottom: '4px',
+
+    '&:hover': {
+        textDecoration: 'underline',
+    },
+});
+
+export const Categories = styled.div({
+    paddingTop: '1rem',
+});
+
+export const WholeTexts = styled.p({
+    paddingBottom: '24px',
+});
+
+export const CategoriesBox = styled.div({});
+
+export const Span = styled.span({
+    paddingTop: '6px',
+    paddingBottom: '6px',
+    fontSize: '28px',
+    borderBottom: '1px solid #5f6365',
+    color: '#ffd559',
+    display: 'block',
+});
+
+export const Ul = styled.ul({
+    padding: '8px 0 8px 0',
+});
+
+export const Li = styled.li({
+    // color: '#fff2cc', // TODO: 테마 변경시 이색상으로 변경
+    color: '#300B0B',
+    padding: '4px',
+});
 
 export default function Sidebar() {
     const image =
@@ -25,16 +68,85 @@ export default function Sidebar() {
         <Container>
             <Profile>
                 <ProfileImage src={image} alt="" />
-                <p>Sidebar</p>
+                <Div>
+                    <p>Title</p>
+                    <p>Description</p>
+                </Div>
+
+                <A href="https://www.naver.com" target="__blank">
+                    <img src="" alt="" />
+                    Email
+                </A>
+                <A href="example" target="__blank">
+                    <img src="" alt="" />
+                    GitHub
+                </A>
             </Profile>
             <Categories>
-                <ul>
-                    <li>list 1</li>
-                    <li>list 2</li>
-                    <li>list 3</li>
-                    <li>list 4</li>
-                    <li>list 5</li>
-                </ul>
+                <WholeTexts>
+                    <img src="" alt="" />
+                    전체 글 수 999개
+                </WholeTexts>
+                <CategoriesBox>
+                    <Span>TypeScript</Span>
+                    <Ul>
+                        <Li>
+                            <Link to="/">List 1 (150개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 2 (123개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 3 (60개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 4 (13개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 5 (3개)</Link>
+                        </Li>
+                    </Ul>
+                </CategoriesBox>
+                <CategoriesBox>
+                    <Span>Python</Span>
+                    <Ul>
+                        <Li>
+                            <Link to="/">List 1 (150개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 2 (123개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 3 (60개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 4 (13개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 5 (3개)</Link>
+                        </Li>
+                    </Ul>
+                </CategoriesBox>
+                <CategoriesBox>
+                    <Span>Go</Span>
+                    <Ul>
+                        <Li>
+                            <Link to="/">List 1 (150개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 2 (123개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 3 (60개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 4 (13개)</Link>
+                        </Li>
+                        <Li>
+                            <Link to="/">List 5 (3개)</Link>
+                        </Li>
+                    </Ul>
+                </CategoriesBox>
             </Categories>
         </Container>
     );
