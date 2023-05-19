5a60bd5b3b83b5e98ebb985e7b7f4a38ea8ee4ca
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-19 16:14:39 +0900

    Install `@esbuild-plugins/node-globals-polyfill`

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: '/tack-blog',
   build: {
 	  assetsInlineLimit: 0,
-  }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                buffer: true
+            })
+        ]
+    }
+}
 });
