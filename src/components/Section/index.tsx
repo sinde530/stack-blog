96dd17955bdd8cebd0637dc42c7714628b67291e
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-19 13:10:25 +0900

    Add `calender.png`

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,14 +1,27 @@
 import styled from '@emotion/styled';
+import Heading from 'src/common/Heading';
+import calenderImage from 'src/assets/images/calendar.png';
 
 export const Container = styled.div({
-    backgroundColor: 'blue',
+    // backgroundColor: 'blue',
     padding: '12px 12px',
 });
 
 export default function Section() {
     return (
         <Container>
-            <p>Section</p>
+            <Heading>Resent Posts</Heading>
+
+            <PostContainer>
+                <TodayBox>
+                    <CalenderImage
+                        src={calenderImage}
+                        alt={`${calenderImage} error`}
+                    />
+                    <WirteDate>2023-05-19</WirteDate>
+                </TodayBox>
+                <p>Section</p>
+            </PostContainer>
         </Container>
     );
 }
