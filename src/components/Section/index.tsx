038371ebb33c70bfeaa343844b0f7ab1a05451a2
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-19 14:34:05 +0900

    Add `styled.ts`

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,58 +1,16 @@
-import styled from '@emotion/styled';
 import Heading from 'src/common/Heading';
 import calenderImage from 'src/assets/images/calendar.png';
 import { Link } from 'react-router-dom';
-
-export const Container = styled.div({
-    // backgroundColor: 'blue',
-    padding: '12px 12px',
-    width: 'calc(100% - 300px)',
-});
-
-export const PostContainer = styled.div({
-    padding: '16px 0 16px 0',
-});
-
-export const TodayBox = styled.div({
-    display: 'flex',
-});
-
-export const CalenderImage = styled.img({
-    width: '16px',
-    height: '16px',
-    marginRight: '8px',
-});
-
-export const WirteDate = styled.p({
-    fontSize: '16px',
-});
-
-export const Title = styled.p({
-    fontSize: '20px',
-    fontWeight: '600',
-    paddingTop: '8px',
-    paddingBottom: '16px',
-});
-
-export const TagBox = styled.div({
-    display: 'flex',
-});
-
-export const TagText = styled(Link)({
-    padding: '3px 8px',
-    marginRight: '6px',
-    marginBottom: '8px',
-    border: '1px solid #373C3F',
-    borderRadius: '4px',
-    backgroundColor: '#6d7275',
-    color: '#ffc005',
-    zIndex: '99999',
-
-    '&:hover': {
-        backgroundColor: '#000',
-        color: '#ffc005',
-    },
-});
+import {
+    CalenderImage,
+    Container,
+    PostContainer,
+    TagBox,
+    TagText,
+    Title,
+    TodayBox,
+    WirteDate,
+} from './styled';
 
 export default function Section() {
     return (
