7c65d0880e64c8b5e8f5146eed083d07a7fb976e
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-19 17:43:54 +0900

    Create `today.ts`

---
+++ src/common/today.ts
@@ -0,0 +1,8 @@
+export const today = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const date = String(currentDate.getDate()).padStart(2, '0');
+    const yearMonthDate = `${year}-${month}-${date}`;
+    return yearMonthDate;
+};
