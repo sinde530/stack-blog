56e4deb12f612fdc67e6fcb0872daed93def2f7f
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-23 13:27:29 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import MainLayout from './layouts/MainLayout';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
 import Create from './components/Create';
+import Posts from './components/Posts';
 
 export default function App() {
     return (
         <Routes>
             <Route element={<MainLayout />}>
-                <Route path="/tack-blog/" element={<Outlet />}>
+                <Route path="/tack-blog" element={<Outlet />}>
+                    <Route
+                        path="/tack-blog/posts/:folderName/:fileName/:title"
+                        element={<Posts />}
+                    />
+
                     <Route index element={<Home />} />
                 </Route>
             </Route>
