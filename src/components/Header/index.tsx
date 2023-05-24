7937eea5684cfe94d0abc3f66790062e5e903b60
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-24 12:47:00 +0900

    Add `Header` Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,83 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import logo from 'src/assets/images/logo.png';
+
+export const HeaderContainer = styled.header({
+    width: '100%',
+    minHeight: '64px',
+    maxWidth: '100%',
+    display: 'flex',
+    fontSize: '20px',
+    justifyContent: 'space-between',
+    borderBottom: '1px solid #000',
+    alignItems: 'center',
+});
+export const HeaderLeftBox = styled.div({
+    padding: '16px 16px',
+});
+export const HeaderRightBox = styled.div({
+    display: 'flex',
+});
+export const LogoText = styled(Link)({
+    display: 'flex',
+
+    '&:hover': {
+        // color: '#ced4da',
+        fontWeight: '600',
+    },
+});
+export const LogoImage = styled.img({
+    color: 'gray',
+    width: '24px',
+    height: '24px',
+    filter: 'invert(44%) sepia(64%) saturate(796%) hue-rotate(151deg) brightness(101%) contrast(105%)',
+});
+export const ListItem = styled.ul({
+    display: 'flex',
+});
+export const Item = styled.li({
+    padding: '16px 16px',
+});
+
+export const LinkTo = styled(Link)({
+    position: 'relative',
+
+    '&:before': {
+        content: '""',
+        position: 'absolute',
+        left: 0,
+        bottom: 0,
+        height: '4px',
+        background: '#ffbf00',
+        width: '100%',
+        transform: 'scaleX(0)',
+        transformOrigin: 'left',
+        transition: 'all 0.2s ease-in-out',
+    },
+    '&:hover:before': {
+        transform: 'scaleX(1)',
+    },
+});
+
+export default function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderLeftBox>
+                <LogoText to="/tack-blog/">
+                    sinde530 tack blog. &zwj;
+                    <LogoImage src={logo} alt={`${logo} error`} />
+                </LogoText>
+            </HeaderLeftBox>
+            <HeaderRightBox>
+                <ListItem>
+                    <Item>
+                        <LinkTo to="/tack-blog/">Home</LinkTo>
+                    </Item>
+                    <Item>
+                        <LinkTo to="/tack-blog/category">Category</LinkTo>
+                    </Item>
+                </ListItem>
+            </HeaderRightBox>
+        </HeaderContainer>
+    );
+}
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,70 +1,12 @@
 import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
-
-import logo from 'src/assets/images/logo.png';
 
 import Sidebar from '../Sidebar';
 import PostList from '../PostList';
+import Header from '../Header';
 
 export const Container = styled.div({
     maxWidth: '1440px',
     margin: '0 auto',
-});
-export const Header = styled.header({
-    width: '100%',
-    minHeight: '64px',
-    maxWidth: '100%',
-    display: 'flex',
-    fontSize: '20px',
-    justifyContent: 'space-between',
-    borderBottom: '1px solid #000',
-    alignItems: 'center',
-});
-export const HeaderLeftBox = styled.div({
-    padding: '16px 16px',
-});
-export const HeaderRightBox = styled.div({
-    display: 'flex',
-});
-export const LogoText = styled(Link)({
-    display: 'flex',
-
-    '&:hover': {
-        // color: '#ced4da',
-        fontWeight: '600',
-    },
-});
-export const LogoImage = styled.img({
-    color: 'gray',
-    width: '24px',
-    height: '24px',
-    filter: 'invert(44%) sepia(64%) saturate(796%) hue-rotate(151deg) brightness(101%) contrast(105%)',
-});
-export const ListItem = styled.ul({
-    display: 'flex',
-});
-export const Item = styled.li({
-    padding: '16px 16px',
-});
-
-export const LinkTo = styled(Link)({
-    position: 'relative',
-
-    '&:before': {
-        content: '""',
-        position: 'absolute',
-        left: 0,
-        bottom: 0,
-        height: '4px',
-        background: '#ffbf00',
-        width: '100%',
-        transform: 'scaleX(0)',
-        transformOrigin: 'left',
-        transition: 'all 0.2s ease-in-out',
-    },
-    '&:hover:before': {
-        transform: 'scaleX(1)',
-    },
 });
 
 export const MainContents = styled.div({
@@ -89,24 +31,7 @@ type PostListProps = {
 export default function Home({ posts }: PostListProps) {
     return (
         <Container>
-            <Header>
-                <HeaderLeftBox>
-                    <LogoText to="/tack-blog/">
-                        sinde530 tack blog. &zwj;
-                        <LogoImage src={logo} alt={`${logo} error`} />
-                    </LogoText>
-                </HeaderLeftBox>
-                <HeaderRightBox>
-                    <ListItem>
-                        <Item>
-                            <LinkTo to="/tack-blog/">Home</LinkTo>
-                        </Item>
-                        <Item>
-                            <LinkTo to="/tack-blog/category">Category</LinkTo>
-                        </Item>
-                    </ListItem>
-                </HeaderRightBox>
-            </Header>
+            <Header />
             <MainContents>
                 <Sidebar />
                 <PostList posts={posts} />
