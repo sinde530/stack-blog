9cbcc22c2218730a9963badd54d6c6694dca2717
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-26 01:17:37 +0900

    Add `md plugins`

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import md from 'vite-plugin-md'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), md()],
   base: '/tack-blog',
   build: {
     assetsInlineLimit: 0,
