6b63e8267d83998a52aa965598492a9f42be63a5
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-05-31 12:59:16 +0900

    Add Sidebar Toggle

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,14 +1,20 @@
 /* eslint-disable react/no-array-index-key */
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 import { Link } from 'react-router-dom';
 import postsData from 'src/postsData';
 
-export const Container = styled.div({
-    width: 'calc(280px - 1em)',
+type ContainerProps = {
+    sidebarVisible: boolean;
+};
+
+export const Container = styled.div<ContainerProps>(({ sidebarVisible }) => ({
+    width: sidebarVisible ? 'calc(280px - 1em)' : '0',
     overflowY: 'auto',
     height: 'calc(100vh - 2em - 2em)',
-    padding: '18px 18px',
+    padding: sidebarVisible ? '18px 18px' : '0',
+    transition: 'width 0.3s ease',
 
     '&::-webkit-scrollbar': {
         width: '8px',
@@ -17,7 +23,7 @@ export const Container = styled.div({
         backgroundColor: '#888',
         borderRadius: '4px',
     },
-});
+}));
 
 export const Div = styled.div({
     paddingTop: '1rem',
@@ -66,62 +72,86 @@ export const Ul = styled.ul({
 });
 
 export const Li = styled.li({
-    // color: '#fff2cc', // TODO: 테마 변경시 이색상으로 변경
     color: '#300B0B',
     padding: '4px',
+});
+
+export const ToggleButton = styled.button({
+    position: 'absolute',
+    display: 'none',
+    top: '1rem',
+    right: '1rem',
+    padding: '0.5rem',
+    background: 'none',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '1.5rem',
+
+    '@media (max-width: 750px)': {
+        display: 'block',
+    },
 });
 
 export default function Sidebar() {
     const image =
         'https://dummyimage.com/1000x800/000000/fff&text=Prifle+Image.';
     const totalPosts = postsData.length;
-
     const uniqueCategories = [
         ...new Set(postsData.map((post) => post.categories)),
     ];
+    const [sidebarVisible, setSidebarVisible] = useState(true);
+
+    const toggleSidebar = () => {
+        setSidebarVisible(!sidebarVisible);
+    };
 
     return (
-        <Container>
-            <Profile>
-                <ProfileImage src={image} alt="" />
-                <Div>
-                    <p>Title</p>
-                    <p>Description</p>
-                </Div>
+        <>
+            <ToggleButton onClick={toggleSidebar}>&#9776;</ToggleButton>
+            <Container sidebarVisible={sidebarVisible}>
+                <Profile>
+                    <ProfileImage src={image} alt="" />
+                    <Div>
+                        <p>Title</p>
+                        <p>Description</p>
+                    </Div>
 
-                <A href="https://www.naver.com" target="__blank">
-                    <img src="" alt="" />
-                    Email
-                </A>
-                <A href="example" target="__blank">
-                    <img src="" alt="" />
-                    GitHub
-                </A>
-            </Profile>
-            <Categories>
-                <WholeTexts>
-                    <img src="" alt="" />
-                    전체 글 수 {totalPosts}개
-                </WholeTexts>
-                {uniqueCategories.map((category) => (
-                    <CategoriesBox key={category}>
-                        <Span>{category}</Span>
-                        <Ul>
-                            {postsData
-                                .filter((post) => post.categories === category)
-                                .map((post, index) => (
-                                    <Li key={index}>
-                                        <Link
-                                            to={`posts/${post.categories}/${post.fileName}`}
-                                        >
-                                            {post.title}
-                                        </Link>
-                                    </Li>
-                                ))}
-                        </Ul>
-                    </CategoriesBox>
-                ))}
-            </Categories>
-        </Container>
+                    <A href="https://www.naver.com" target="__blank">
+                        <img src="" alt="" />
+                        Email
+                    </A>
+                    <A href="example" target="__blank">
+                        <img src="" alt="" />
+                        GitHub
+                    </A>
+                </Profile>
+                <Categories>
+                    <WholeTexts>
+                        <img src="" alt="" />
+                        전체 글 수 {totalPosts}개
+                    </WholeTexts>
+                    {uniqueCategories.map((category) => (
+                        <CategoriesBox key={category}>
+                            <Span>{category}</Span>
+                            <Ul>
+                                {postsData
+                                    .filter(
+                                        (post) => post.categories === category,
+                                    )
+                                    .map((post, index) => (
+                                        <Li key={index}>
+                                            <Link
+                                                to={`posts/${post.categories}/${post.fileName}`}
+                                            >
+                                                {post.title}
+                                            </Link>
+                                        </Li>
+                                    ))}
+                            </Ul>
+                        </CategoriesBox>
+                    ))}
+                </Categories>
+            </Container>
+        </>
     );
 }
