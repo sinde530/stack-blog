a4d83472b744b2b09400f0c1c8ac70e85a8de423
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-06-02 17:42:41 +0900

    Add Footer `Container` Styled

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,16 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div({
+    padding: '18px 18px',
+    marginTop: '2rem',
+});
+
 export default function Footer() {
     return (
-        <footer>
+        <Container>
             <p>footer footer footer</p>
             <p>footer footer footer</p>
             <p>footer footer footer</p>
-        </footer>
+        </Container>
     );
 }
