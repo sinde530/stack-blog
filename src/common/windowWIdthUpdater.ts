305769d4985b2e2464e3058b8d8ee32e4e8c2cc2
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-06-07 09:59:36 +0900

    Add `windowWIdthUpdate.ts`
    mediaQuery사용하지 않고, 코드로만 작동할수있는 방안 생성.

---
+++ src/common/windowWIdthUpdater.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export const windowWIdthUpdate = () => {
+    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return windowWidth;
+};
