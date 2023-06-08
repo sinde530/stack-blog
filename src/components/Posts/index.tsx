de8cc4bd6147d391e7de93335013d019ec15acf7
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-06-08 16:02:58 +0900

    Update `Posts` Component

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
 import axios from 'axios';
 import styled from '@emotion/styled';
+import matter from 'gray-matter';
 
 export const Container = styled.div({
     padding: '12px 12px',
@@ -20,7 +21,7 @@ const MarkdownStyle = styled.div({
     color: '#333',
     h1: {
         fontSize: '2.5em',
-        borderBottom: '1px solid #eaecef',
+        // borderBottom: '1px solid #eaecef',
     },
     h2: {
         fontSize: '2em',
@@ -42,17 +43,23 @@ const MarkdownStyle = styled.div({
             color: '#d63384',
         },
     },
+    hr: {
+        display: 'none',
+    },
 });
 
 export type ParamProps = {
     categories: string;
     fileName: string;
+    title: string;
+    date: string;
 };
 
 export default function Posts() {
-    const { categories, fileName } = useParams<ParamProps>();
+    const { categories, fileName, title, date } = useParams<ParamProps>();
     const [mdSource, setMdSource] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [metaData, setMetadata] = useState<any>(null);
     const url = `https://raw.githubusercontent.com/sinde530/tack-blog/master/public/posts/${categories}/${fileName}.md`;
     const localUrl = `http://localhost:5173/tack-blog/posts/${categories}/${fileName}.md`;
 
@@ -67,8 +74,12 @@ export default function Posts() {
                 } catch {
                     response = await axios.get(localUrl);
                 }
+                const { data } = response;
+                const parsedMarkdown = matter(data);
 
-                setMdSource(response.data);
+                // setMdSource(response.data);
+                setMdSource(parsedMarkdown.content);
+                setMetadata(parsedMarkdown.data);
             } catch (error) {
                 console.error('Failed to fetch post:', error);
             } finally {
@@ -77,7 +88,7 @@ export default function Posts() {
         };
 
         fetchPostContent();
-    }, [categories, fileName]);
+    }, [categories, fileName, title, date]);
 
     return (
         <Container>
@@ -88,6 +99,19 @@ export default function Posts() {
             ) : (
                 mdSource && (
                     <>
+                        {metaData && (
+                            <div>
+                                <p>{metaData.title}</p>
+                                <p>{metaData.date}</p>
+                                <p>{metaData.author}</p>
+                                <div>
+                                    {metaData.tags.map((tag: string) => (
+                                        <p key={tag}>{tag}</p>
+                                    ))}
+                                </div>
+                                <p>{metaData.categories}</p>
+                            </div>
+                        )}
                         <MarkdownStyle>
                             <ReactMarkdown
                                 className="markdown"
@@ -95,10 +119,6 @@ export default function Posts() {
                             >
                                 {mdSource}
                             </ReactMarkdown>
-
-                            <h1>
-                                {categories}/{fileName}
-                            </h1>
                         </MarkdownStyle>
                         <div>
                             <Link to="/tack-blog">Home</Link>
