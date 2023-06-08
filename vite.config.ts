faf5066e004f1351c5adeb433587fb519c8c440a
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-06-08 17:34:55 +0900

    Add vite.config.ts 'polyfillNode()'

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import polyfillNode from 'rollup-plugin-polyfill-node';
+
 import md from 'vite-plugin-md'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), md()],
+  plugins: [react(), tsconfigPaths(), md(), polyfillNode()],
   base: '/tack-blog',
   build: {
     assetsInlineLimit: 0,
